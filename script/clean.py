256016f92746bbe52b0d9801ff507d334f2ab916
Any <anyarlene.niyubahwe@hotmail.com>
2022-03-22 22:05:37 +0100

    update script to get 5 letter words

--- script/clean.py
+++ script/clean.py
@@ -3,7 +3,7 @@ from os import sep
 import unidecode
 import pandas as pd
 
-path = '/Users/niyubahweany-arlene/kirundi-words/'
+path = ''
 file = open(path + "amajambo.csv")
 csvreader = csv.reader(file)
 
@@ -16,25 +16,46 @@ for sublist in rows:
     for item in sublist:
         flat_list.append(item)
 
-words = [item.upper() for item in flat_list]
+words_list = [item.upper() for item in flat_list]
 
 bad_chars = ['!', "-"]
 
-for i in range(len(words)):
+for i in range(len(words_list)):
     # remove ascents
-    words[i] = unidecode.unidecode(words[i])
-    words[i] = ''.join((filter(lambda i: i not in bad_chars, words[i])))
+    words_list[i] = unidecode.unidecode(words_list[i])
+    words_list[i] = ''.join((filter(lambda i: i not in bad_chars, words_list[i])))
 
-#print(words)
-
+#save all clean words in csv file
 with open('amajambo_cleanedV1.csv', 'w', newline='') as f:
   csv_out = csv.writer(f)
-  csv_out.writerows([words[index]] for index in range(0, len(words)))
+  csv_out.writerows([words_list[index]] for index in range(0, len(words_list)))
 
+#count number of words in our list
 def get_number_of_elements(list):
     count = 0
     for element in list:
         count += 1
     return count
 
-print("Number of elements in the list: ", get_number_of_elements(words))
+print("Number of elements in the entire list: ", get_number_of_elements(words_list))
+
+#letters_count = [len(i) for i in words_list]
+
+#print(letters_count)
+
+# find words with only 5 letters
+def five_letter(words):
+    new_wordlist = []
+    for item in words:
+        if len(item) == 5:
+            new_wordlist += [item]
+    return new_wordlist 
+
+res = five_letter(words_list)
+
+print("Number of elements in our new list of 5 letter words: ", get_number_of_elements(res))
+#save all clean words in csv file
+with open('amajambo_5letters.csv', 'w', newline='') as f:
+  csv_out = csv.writer(f)
+  csv_out.writerows([words_list[index]] for index in range(0, len(res)))
+
